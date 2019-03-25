4eb816b3f72ef641eb2c168b1504565150874483
Matthew Powelson <matthew.powelson@swri.org>
2019-03-25 13:16:22 -0500

    Move post processors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
   
 # fetch values from package.xml  
 setup_args = generate_distutils_setup(  
-    packages=['ros_robodk_postprocessors', 'robodk_postprocessors'],
+    packages=['ros_robodk_postprocessors', 'ros_robodk_postprocessors.robodk_postprocessors'],
     package_dir={'': 'src'},  
 )  
   
