eeb287328581e653d4b65c554e007d95a01143b8
mripperger <michael.ripperger@swri.org>
2019-04-01 17:00:04 -0500

    Moved robodk_post_processors back into top level package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
   
 # fetch values from package.xml  
 setup_args = generate_distutils_setup(  
-    packages=['ros_robodk_post_processors', 'robodk_post_processors'],
+    packages=['ros_robodk_post_processors', 'ros_robodk_post_processors.robodk_post_processors'],
     package_dir={'': 'src'},  
 )  
   
